0aafb0c7d74d9322c0a9742e46ceb4046454059c
CTRSamatSadvakassov <ssadvakassov@nucleussec.com>
2025-12-14 11:00:16 +0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+def print_hello():
+    """Print a hello world message"""
+    message = "Hello World"
+    print(message)
+    return True
+
+# Main execution
+if __name__ == "__main__":
+    result = print_hello()
+    if result = True:
+        print("Success!")
